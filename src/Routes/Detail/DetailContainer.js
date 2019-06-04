6dd243bb578087f69e4465cbb7d1b9ab21fdd7af
Dasol Park <dasol.park615@gmail.com>
2019-06-04 22:49:44 +0800

    5-5. Detail Container part One

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -8,6 +8,20 @@ export default class extends React.Component {
     loading: true
   };
 
+  async componentDidMount() {
+    const {
+      match: {
+        params: { id }
+      },
+      history: { push }
+    } = this.props;
+    const parsedId = parseInt(id);
+    if (isNaN(parsedId)) {
+      return push("/");
+    }
+    console.log(this.props);
+  }
+
   render() {
     const { result, error, loading } = this.state;
     return <DetailPresenter result={result} error={error} loading={loading} />;
