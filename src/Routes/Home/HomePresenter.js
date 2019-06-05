73cbb6d5f7679b36c04a5c7ea6016fef4dfc8e21
Dasol Park <dasol.park615@gmail.com>
2019-06-05 20:07:45 +0800

    6-0. Presenter Structure

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,1 +1,16 @@
-export default () => "Home";
+import React from "react";
+import propTypes from "prop-types";
+import styled from "styled-components";
+
+const HomePresenter = ({ nowPlaying, upcoming, popular, error, loading }) =>
+  null;
+
+HomePresenter.propTypes = {
+  nowPlaying: propTypes.array,
+  upcoming: propTypes.array,
+  popular: propTypes.array,
+  error: propTypes.string,
+  loading: propTypes.bool.isRequired
+};
+
+export default HomePresenter;
--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -1,1 +1,23 @@
-export default () => "Search";
+import React from "react";
+import propTypes from "prop-types";
+import styled from "styled-components";
+
+const SearchPresenter = ({
+  movieResults,
+  tvResults,
+  searchTerm,
+  handleSubmit,
+  loading,
+  error
+}) => null;
+
+SearchPresenter.propTypes = {
+  movieResults: propTypes.array,
+  tvResults: propTypes.array,
+  searchTerm: propTypes.string,
+  handleSubmit: propTypes.func.isRequired,
+  loading: propTypes.bool.isRequired,
+  error: propTypes.string
+};
+
+export default SearchPresenter;
