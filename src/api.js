6f9305ced8fa4f878b34094e356893c33627c7a4
Dasol Park <dasol.park615@gmail.com>
2019-06-05 22:09:55 +0800

    Modified API(axios)

--- src/api.js
+++ src/api.js
@@ -1,44 +1,65 @@
 import axios from "axios";
 
+const params = {
+  api_key: "f596dd7d9a5491647a16554b105ed63c",
+  language: "en-US"
+};
+
 const api = axios.create({
-  baseURL: "https://api.themoviedb.org/3/",
-  params: {
-    api_key: "f596dd7d9a5491647a16554b105ed63c",
-    language: "en-US"
-  }
+  baseURL: "https://api.themoviedb.org/3/"
 });
 
 export const moviesApi = {
-  nowPlaying: () => api.get("movie/now_playing"),
-  upcoming: () => api.get("movie/upcoming"),
-  popular: () => api.get("movie/popular"),
+  nowPlaying: () =>
+    api.get("movie/now_playing", {
+      params
+    }),
+  upcoming: () =>
+    api.get("movie/upcoming", {
+      params
+    }),
+  popular: () =>
+    api.get("movie/popular", {
+      params
+    }),
   movieDetail: id =>
     api.get(`movie/${id}`, {
       params: {
+        api_key: params.api_key,
+        language: params.language,
         append_to_response: "videos"
       }
     }),
   search: term =>
     api.get("search/movie", {
       params: {
+        api_key: params.api_key,
+        language: params.language,
         query: encodeURIComponent(term)
       }
     })
 };
 
 export const tvApi = {
-  topRated: () => api.get("tv/top_rated"),
+  topRated: () =>
+    api.get("tv/top_rated", {
+      params
+    }),
   popular: () => api.get("tv/popular"),
   airingToday: () => api.get("tv/airing_today"),
   showDetail: id =>
     api.get(`tv/${id}`, {
       params: {
+        api_key: params.api_key,
+        language: params.language,
         append_to_response: "videos"
       }
     }),
   search: term =>
     api.get("search/tv", {
       params: {
+        api_key: params.api_key,
+        language: params.language,
         query: encodeURIComponent(term)
       }
     })
